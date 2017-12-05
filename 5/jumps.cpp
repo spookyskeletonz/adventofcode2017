7dbeed93c3fcef9bf8fb1d5cfa979dd27ad67b2b
Neil.Sarkar <ansarkar@outlook.com>
2017-12-05 20:47:59 +1100

    linear solution for both parts, although may be able to reduce constant but not significant enough for me to spend time on

---
+++ 5/jumps.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unordered_map>
+
+int main(int argc, char **argv){
+    std::ifstream filein(argv[1]);
+    int countpos = 0;
+    int currentjump;
+    std::string scurrentjump;
+    std::unordered_map<int, int> jumps;
+    
+    while(std::getline(filein, scurrentjump)){
+        currentjump = std::stoi(scurrentjump);
+        jumps.insert(std::make_pair(countpos, currentjump));
+        countpos++;
+    }
+    
+    int pos = 0;
+    int steps = 0;
+    while(pos < countpos && pos >= 0){
+        int jump = jumps[pos];
+        int newpos = pos + jump;
+        jumps[pos]++;
+        steps++;
+        pos = newpos;
+    }
+    std::cout << "escaped in " << steps << " steps\n";
+
+
+}

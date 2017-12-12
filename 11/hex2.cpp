e96190ce31bdcf6f26682cd04c3a9316eb83fc91
Neil Sarkar <neil.sarkar@loricahealth.com>
2017-12-12 11:27:02 +1100

    day 11 part 2 solution

---
+++ 11/hex2.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+
+int x = 0; /*  axis: / */
+int y = 0; /* axis: \ */
+int z = 0; /* axis - */
+int maxDist = 0;
+
+void newStep(std::string);
+
+int main(int argc, char **argv){
+    std::ifstream infile(argv[1]);
+    std::string line;
+    std::getline(infile, line);
+    std::string token;
+    size_t pos = 0;
+
+    while((pos = line.find(',')) != std::string::npos){
+        token = line.substr(0, pos);
+        newStep(token);
+        line.erase(0, pos + 1);
+    }
+    newStep(line); 
+    std::cout << maxDist << "\n";
+}
+
+void newStep(std::string direction){
+    if(direction == "s"){
+        x--;
+        y--;
+    } else if(direction == "se"){
+        z++;
+        y--;
+    } else if(direction == "ne"){
+        x++;
+        z++;
+    } else if(direction == "n"){
+        x++;
+        y++;
+    } else if(direction == "nw"){
+        y++;
+        z--;
+    } else if(direction == "sw"){
+        z--;
+        x--;
+    }
+
+    int dist = std::max({std::abs(0-x), std::abs(0-y), std::abs(0-z)});
+    if(dist > maxDist) maxDist = dist;
+}
+
